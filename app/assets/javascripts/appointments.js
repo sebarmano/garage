0d82a719f76d26bdc30ac096ec9591a4c257a5ba
Sebastian Armano <sebarmano@gmail.com>
2015-12-02 16:18:12 -0500

    WIP: select just available hour for the date

--- app/assets/javascripts/appointments.js
+++ app/assets/javascripts/appointments.js
@@ -1,2 +1,18 @@
-$("#appointment_date_on").pickadate();
+$("#appointment_date_on").pickadate({
+  // Days available
+  min: 1,
+  max: 60,
+  disable: [1, 8, 15, 22, 29, 36, 43, 50, 57],
+
+  // Buttons
+  today: "",
+  clear: "Borrar día seleccionado",
+  close: "Cerrar",
+
+  onSet: function(context) {
+    console.log('Just set stuff:', context)
+  }
+});
+
 $("#appointment_starts_at").pickatime();
+
