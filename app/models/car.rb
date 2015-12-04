d2a5d8b9e32921c3f1ef9d4617479a1c09703185
Sebastian Armano <sebarmano@gmail.com>
2015-12-03 23:57:32 -0500

    Fix some tests and add requirement of license to create a car

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,8 +1,10 @@
 class Car < ActiveRecord::Base
   belongs_to :user
-  validates :brand, :model, presence: true
+  validates :brand, presence: true
+  validates :model, presence: true
+  validates :license, presence: true
 
   def description
-    "#{brand.capitalize} #{model.capitalize}"
+    "#{brand.capitalize} #{model.capitalize} #{license.upcase}"
   end
 end
--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Car, type: :model do
   it { is_expected.to belong_to(:user) }
   it { is_expected.to validate_presence_of(:brand) }
   it { is_expected.to validate_presence_of(:model) }
+  it { is_expected.to validate_presence_of(:license) }
+
 
   it "provides a description" do
     car = create(:car,
